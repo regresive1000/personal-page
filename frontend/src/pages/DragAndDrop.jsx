3493fcc72185328bb3dcbc3b18fe8a54ea2a5406
Nicolas Felipe Vanderstichel <nfvander@gmail.com>
2023-06-23 17:40:42 -0300

    change query

--- frontend/src/pages/DragAndDrop.jsx
+++ frontend/src/pages/DragAndDrop.jsx
@@ -200,7 +200,7 @@ const DragAndDrop = () => {
                   <Draggable key={taskCol.id} draggableId={taskCol.id} index={indexCol} type="column">
                     { (draggableColProvider) => (
 
-                      <div className={`${styles.cardTasks} col-12 col-md-3`} {...draggableColProvider.draggableProps} ref={draggableColProvider.innerRef}  >
+                      <div className={`${styles.cardTasks} col-12 col-md`} {...draggableColProvider.draggableProps} ref={draggableColProvider.innerRef}  >
                         <h3 {...draggableColProvider.dragHandleProps} ><div></div>{taskCol.title}<button onClick={e => {handleDeleteColumn(taskCol.id)}}><svg xmlns={xSvg} width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></button></h3>
                         
                         <Droppable droppableId={taskCol.name} type='task'>
